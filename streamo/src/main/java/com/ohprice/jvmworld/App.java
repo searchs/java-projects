184df021c2d81785b06895377efe9281c9cee7e9
Olatunji Ajibode <ola.ajibode@gmail.com>
2019-12-07 09:02:51 +0000

    print ResultSet function added

--- streamo/src/main/java/com/ohprice/jvmworld/App.java
+++ streamo/src/main/java/com/ohprice/jvmworld/App.java
@@ -1,13 +1,41 @@
 package com.ohprice.jvmworld;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 /**
- * Hello world!
- *
+ * JsonToJava
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String user = "root"; //of course this is read from config
+        String password = "root"; //red from config
+        //com.mysql.cj.jdbc.Driver
+        Class.forName("com.mysql.cj.jdbc.Driver")//com.mysql.cj.jdbc.Driver
+        Connection conn = null;
+
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business", user, password);
+        Statement st = conn.createStatement();
+
+        ResultSet resultSet = st.executeQuery("SELECT * FROM Customerinfo LIMIT 3;");
+
+        while(resultSet.next()) {
+            System.out.println(resultSet.getString(1));
+            System.out.println(resultSet.getString(2));
+            System.out.println(resultSet.getInt(3));
+            System.out.println(resultSet.getString(4));
+        }
+
+        conn.close();
+    }
+
+    private void printCustomerRowData(ResultSet rs) {
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+            System.out.println(rs.getString(2));
+            System.out.println(rs.getInt(3));
+            System.out.println(rs.getString(4));
+        }
     }
 }
